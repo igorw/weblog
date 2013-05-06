5570e5a91de96d0af212f292299e0ca801c69ab7
Igor Wiedler <igor@wiedler.ch>
2013-05-06 19:54:28 +0200

    Remove space padding from day-of-month

--- _plugins/date.rb
+++ _plugins/date.rb
@@ -4,7 +4,7 @@ require 'facets/integer/ordinal'
 module Jekyll
   module DateFilter
     def date_ordinal(date)
-      "#{date.strftime('%b')} #{date.strftime('%e').to_i.ordinalize}, #{date.strftime('%Y')}"
+      "#{date.strftime('%b')} #{date.strftime('%d').to_i.ordinalize}, #{date.strftime('%Y')}"
     end
   end
 end
